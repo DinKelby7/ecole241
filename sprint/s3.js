fbef14cdd02fb50baeaae1726121ce4bc4909263
DinKelby7 <44721093+DinKelby7@users.noreply.github.com>
2019-02-18 13:18:56 +0100

    new s3

---
+++ sprint/s3.js
@@ -0,0 +1,7 @@
+ var nombre = 18;
+            if (nombre>10){
+                console.log('Le nombre est superieur');
+            }
+            else{
+                console.log('Le nombre est pas superieur');
+            }
