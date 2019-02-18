caadb60b38c2e569b5f990be9135beac73dec79f
DinKelby7 <44721093+DinKelby7@users.noreply.github.com>
2019-02-18 14:12:50 +0100

    boom

---
+++ sprint/s5.js
@@ -0,0 +1,9 @@
+ var num1 = 6;
+            var num2 = 7;
+            var question = prompt("Veuillez entrez deux chiffres espacés d'une virgule")
+            if (num1<num2){
+                console.log("7 est plus grand");
+            }
+            else{
+                console.log("6 est plus petit");
+            }
