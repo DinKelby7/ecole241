92dbb988ba0e74aab883cc6972dffbc680284488
DinKelby7 <44721093+DinKelby7@users.noreply.github.com>
2019-02-18 14:31:56 +0100

    s7 exo

---
+++ sprint/s6.js
@@ -0,0 +1,8 @@
+  var age = Number(window.prompt("Entrez votre age"));
+            
+            if(age<=20){
+                console.log("Vous etes mineur mon petit");
+            }
+            else{
+                console.log("Vous etes majeur ");
+            }
