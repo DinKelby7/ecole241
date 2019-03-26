a2affb5053e23139cbc775c1a9414baedd3211b2
DinKelby7 <44721093+DinKelby7@users.noreply.github.com>
2019-03-26 14:47:32 +0100

    ajout du sprint act2 de php

---
+++ sprint/php/act2.php
@@ -0,0 +1,7 @@
+<?php
+$lieu = "Ecole 241";
+$nombre = 44;
+$nom = "Din";
+$profession = "Developpeur";
+echo "<p> Je suis $nom , mon metier c'est $profession et j'apprend a $lieu ou nous somme a $nombre<p>"
+?>
