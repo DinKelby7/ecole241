c1d74e135d2529cf28d1b28f7b2ecad6603c51ab
DinKelby7 <44721093+DinKelby7@users.noreply.github.com>
2019-03-27 10:22:03 +0100

    ajout de l'acte 3 en PHP

--- sprint/php/act2.php
+++ sprint/php/act2.php
@@ -3,5 +3,7 @@ $lieu = "Ecole 241";
 $nombre = 44;
 $nom = "Din";
 $profession = "Developpeur";
-echo " Je suis $nom , mon metier c'est $profession et j'apprend a $lieu ou nous somme a $nombre"
+$resto = "Mc";
+$resto2 = "Do"
+echo " <p>Je suis $nom , mon metier c'est $profession et j'apprend a $lieu ou nous somme a $nombre et pour finir j'aime bien les $resto.$resto2<p>";
 ?>
---
+++ sprint/php/act3.php
@@ -0,0 +1,20 @@
+<!doctype html>
+
+<html>
+    <head>
+        <title>Page Title</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="initial-scale=1.0">
+    </head>
+
+    <body>
+        <?php
+        $okacode = array('Din<br>','Darel<br>','Falana<br>','Boumys<br>','Francois<br>','joe<br>','Grace<br>','Yannick<br>','Hamilton<br>','Didier<br>');
+
+            echo $okacode[3];
+            echo $okacode[6];
+            echo $okacode[1];
+            echo $okacode[9];
+        ?>
+    </body>
+</html>
