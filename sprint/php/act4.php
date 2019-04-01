6ff576b32369cb58eed7ae5d4d983aa807d409d7
DinKelby7 <44721093+DinKelby7@users.noreply.github.com>
2019-04-01 11:53:19 +0100

    ajout de act4 avec le tableau

--- sprint/php/act4.php
+++ sprint/php/act4.php
@@ -1,75 +1,99 @@
+<?php
+$okacode = array(
+    1 => array(
+        'nom' => 'Kelby', 
+        'prenom' => 'Din', 
+        'age' => 21,
+        'profil' => 'dinkelby7.github.io/ecole241/profil/'
+    ),
+    2 => array(
+        'nom' => 'John', 
+        'prenom' => 'Hamilton', 
+        'age' => 25,
+        'profil' =>'hamiltondarryl.github.io/ecole241/profil/'
+    ),
+    3
+    => array(
+        'nom' => 'Tef', 
+        'prenom' => 'Ivan', 
+        'age' => 26,
+        'profil' => 'iavich.github.io/ecole241/profil/'
+    ),
+    4 => array(
+        'nom' => 'Atol', 
+        'prenom' => 'Boumys', 
+        'age' => 28,'profil' => 'jboumys.github.io/ecole241/profil/'
+    ),
+    5 => array(
+        'nom' => 'Tefl', 
+        'prenom' => 'Laurion', 
+        'age' => 24,
+        'profil' => 'TEFL'
+    ),
+    6 => array(
+        'nom' => 'Tchop', 
+        'prenom' => 'Falana', 
+        'age' => 19,
+        'profil' => 'Fapeterie'
+    ),
+    7 => array(
+        'nom' => 'dodo', 
+        'prenom' => 'Kevin', 
+        'age' => 21,
+        'profil' => 'mbela-kevinn98.github.io/ecole241/profil/'
+    ),
+    8 => array(
+        'nom' => 'Boulengibindza',
+        'prenom' => 'Axel', 
+        'age' => 28,
+        'profil' => 'mlraleader.github.io/ecole241/profil/index.html'),
+    9 => array(
+        'nom' => 'Git', 
+        'prenom' => 'Francois', 
+        'age' => 21,
+        'profil' => 'bernyfrancois.github.io/ecole241/profil/'
+    ),
+    10 => array(
+        'nom' => 'Kog',
+        'prenom' => 'Darel', 
+        'age' => 20,
+        'profil' =>'darel14.github.io/ecole241/profil/'
+    )
+);
+
+?> 
 <!doctype html>
 
 <html>
     <head>
         <title>Act4</title>
         <meta charset="UTF-8">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     </head>
 
     <body>
-        <?php
-        $okacode = array(
-            1 => array(
-                'nom' => 'Kelby', 
-                'prenom' => 'Din', 
-                'age' => 21,
-                'profil' => 'dinkelby7.github.io/ecole241/profil/'
-            ),
-            2 => array(
-                'nom' => 'John', 
-                'prenom' => 'Hamilton', 
-                'age' => 25,
-                'profil' =>'hamiltondarryl.github.io/ecole241/profil/'
-            ),
-            3
-            => array(
-                'nom' => 'Tef', 
-                'prenom' => 'Ivan', 
-                'age' => 26,
-                'profil' => 'iavich.github.io/ecole241/profil/'
-            ),
-            4 => array(
-                'nom' => 'Atol', 
-                'prenom' => 'Boumys', 
-                'age' => 28,'profil' => 'jboumys.github.io/ecole241/profil/'
-            ),
-            5 => array(
-                'nom' => 'Tefl', 
-                'prenom' => 'Laurion', 
-                'age' => 24,
-                'profil' => 'TEFL'
-            ),
-            6 => array(
-                'nom' => 'Tchop', 
-                'prenom' => 'Falana', 
-                'age' => 19,
-                'profil' => 'Fapeterie'
-            ),
-            7 => array(
-                'nom' => 'dodo', 
-                'prenom' => 'Kevin', 
-                'age' => 21,
-                'profil' => 'mbela-kevinn98.github.io/ecole241/profil/'
-            ),
-            8 => array(
-                'nom' => 'Boulengibindza',
-                'prenom' => 'Axel', 
-                'age' => 28,
-                'profil' => 'mlraleader.github.io/ecole241/profil/index.html'),
-            9 => array(
-                'nom' => 'Git', 
-                'prenom' => 'Francois', 
-                'age' => 21,
-                'profil' => 'bernyfrancois.github.io/ecole241/profil/'
-            ),
-            10 => array(
-                'nom' => 'Kog',
-                'prenom' => 'Darel', 
-                'age' => 20,
-                'profil' =>'darel14.github.io/ecole241/profil/'
-            )
-        );
-        var_dump($okacode);
-        ?> 
+        <table class="table table-dark">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nom</th>
+                    <th scope="col">Prenom</th>
+                    <th scope="col">Age</th>
+                    <th scope="col">Profil</th>
+
+                </tr>
+            </thead>
+            <tbody>
+                <?php for($i = 0;$i < count($okacode); $i++): ?>
+                <tr>
+                    <td><?= $i+1 ?></td>
+                    <td><?= $okacode[$i]['prenom']?></td>
+                    <td><?= $okacode[$i]['nom'] ?></td>
+                    <td><?= $okacode[$i]['age'] ?> ans</td>
+                    <td><a href="<?= $okacode[$i]['profil'] ?>" target="_blank" class="btn btn-primary">Consulter</a></td>
+                </tr>
+                <?php endfor; ?>
+            </tbody>
+        </table> 
     </body>
 </html>
