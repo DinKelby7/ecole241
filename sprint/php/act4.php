a955e943b30079e6d7eaccb815cda843ed32653d
DinKelby7 <44721093+DinKelby7@users.noreply.github.com>
2019-04-01 09:42:40 +0100

    organisation du tableau act4php

--- sprint/php/act4.php
+++ sprint/php/act4.php
@@ -9,16 +9,65 @@
     <body>
         <?php
         $okacode = array(
-            1 => array('nom' => 'Kelby', 'prenom' => 'Din', 'age' => '21','profil' => 'dinkelby7.github.io/ecole241/profil/'),
-            2 => array('nom' => 'John', 'prenom' => 'Hamilton', 'age' => '25','profil' =>'hamiltondarryl.github.io/ecole241/profil/'),
-            3 => array('nom' => 'Tef', 'prenom' => 'Ivan', 'age' => '26','profil' => 'iavich.github.io/ecole241/profil/'),
-            4 => array('nom' => 'Atol', 'prenom' => 'Boumys', 'age' => '28','profil' => 'jboumys.github.io/ecole241/profil/'),
-            5 => array('nom' => 'Tefl', 'prenom' => 'Laurion', 'age' => 'hum','profil' => 'TEFL'),
-            6 => array('nom' => 'Tchop', 'prenom' => 'Falana', 'age' => '19','profil' => 'Fapeterie'),
-            7 => array('nom' => 'dodo', 'prenom' => 'Kevin', 'age' => '21','profil' => 'mbela-kevinn98.github.io/ecole241/profil/'),
-            8 => array('nom' => 'Boulengibindza', 'prenom' => 'Axel', 'age' => '28','profil' => 'mlraleader.github.io/ecole241/profil/index.html'),
-            9 => array('nom' => 'Git', 'prenom' => 'Francois', 'age' => '21','profil' => 'bernyfrancois.github.io/ecole241/profil/'),
-            10 => array('nom' => 'Kog', 'prenom' => 'Darel', 'age' => '20','profil' =>'darel14.github.io/ecole241/profil/')
+            1 => array(
+                'nom' => 'Kelby', 
+                'prenom' => 'Din', 
+                'age' => 21,
+                'profil' => 'dinkelby7.github.io/ecole241/profil/'
+            ),
+            2 => array(
+                'nom' => 'John', 
+                'prenom' => 'Hamilton', 
+                'age' => 25,
+                'profil' =>'hamiltondarryl.github.io/ecole241/profil/'
+            ),
+            3
+            => array(
+                'nom' => 'Tef', 
+                'prenom' => 'Ivan', 
+                'age' => 26,
+                'profil' => 'iavich.github.io/ecole241/profil/'
+            ),
+            4 => array(
+                'nom' => 'Atol', 
+                'prenom' => 'Boumys', 
+                'age' => 28,'profil' => 'jboumys.github.io/ecole241/profil/'
+            ),
+            5 => array(
+                'nom' => 'Tefl', 
+                'prenom' => 'Laurion', 
+                'age' => 24,
+                'profil' => 'TEFL'
+            ),
+            6 => array(
+                'nom' => 'Tchop', 
+                'prenom' => 'Falana', 
+                'age' => 19,
+                'profil' => 'Fapeterie'
+            ),
+            7 => array(
+                'nom' => 'dodo', 
+                'prenom' => 'Kevin', 
+                'age' => 21,
+                'profil' => 'mbela-kevinn98.github.io/ecole241/profil/'
+            ),
+            8 => array(
+                'nom' => 'Boulengibindza',
+                'prenom' => 'Axel', 
+                'age' => 28,
+                'profil' => 'mlraleader.github.io/ecole241/profil/index.html'),
+            9 => array(
+                'nom' => 'Git', 
+                'prenom' => 'Francois', 
+                'age' => 21,
+                'profil' => 'bernyfrancois.github.io/ecole241/profil/'
+            ),
+            10 => array(
+                'nom' => 'Kog',
+                'prenom' => 'Darel', 
+                'age' => 20,
+                'profil' =>'darel14.github.io/ecole241/profil/'
+            )
         );
         var_dump($okacode);
         ?> 
